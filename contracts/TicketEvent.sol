de839d115ac10476d58d321ecde9dff53edd3dfb
Edward Kim <itgma1231@gmail.com>
2024-04-08 18:13:43 -0400

    TicketEvent.sol - address public owner; function to list an event

--- contracts/TicketEvent.sol
+++ contracts/TicketEvent.sol
@@ -4,6 +4,46 @@ pragma solidity ^0.8.10;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TicketEvent is ERC721 {
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    address public owner;
+    uint256 public totalOccasions;
+
+    mapping(uint => Occasion) public occasion;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
+
+    function listEvent(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public {
+        totalOccasions++;
+
+        occasion[totalOccasions] = Occasion(
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _maxTickets,
+            _date,
+            _time,
+            _location
+        ); 
+    }
 }
 
--- test/TicketEvent.js
+++ test/TicketEvent.js
@@ -2,8 +2,11 @@ const { expect } = require('chai');
 
 describe('TicketEvent', () => {
   let ticketEvent;
+  let deployer, buyer;
 
   beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners();
+
     const contractFactory = await ethers.getContractFactory('TicketEvent');
     ticketEvent = await contractFactory.deploy('TicketEvent', 'TICK');
   });
@@ -17,6 +20,11 @@ describe('TicketEvent', () => {
     it('Sets token symbol', async () => {
       const symbol = await ticketEvent.symbol();
       expect(symbol).to.eq('TICK');
+    });
+
+    it('Sets the contract owner address', async () => {
+      const ownerAddress = await ticketEvent.owner();
+      expect(ownerAddress).to.eq(deployer.address);
     });
   });
 });
