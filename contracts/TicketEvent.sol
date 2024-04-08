114dbda2b4fcef593305e1dd1ac7b1d8203d5cd7
Edward Kim <itgma1231@gmail.com>
2024-04-08 01:50:09 -0400

    TicketEvent.js test start

--- contracts/TicketEvent.sol
+++ contracts/TicketEvent.sol
@@ -3,5 +3,7 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract TicketEvent is ERC721 {}
+contract TicketEvent is ERC721 {
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+}
 
