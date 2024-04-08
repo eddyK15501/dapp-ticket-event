41c83f021c8c1a0c0f7ca1e70778943c217b5ca1
Edward Kim <itgma1231@gmail.com>
2024-04-08 01:59:38 -0400

    test ERC721 contract name/symbol; package.json test script

--- test/TicketEvent.js
+++ test/TicketEvent.js
@@ -1,3 +1,16 @@
 const { expect } = require('chai');
 
-describe('TicketEvent', () => {})
+describe('TicketEvent', () => {
+    describe('Deployment', () => {
+        it('Sets token name', async () => {
+            const contractFactory = await ethers.getContractFactory('TicketEvent');
+            const ticketEvent = await contractFactory.deploy('TicketEvent', 'TICK');
+
+            const name = await ticketEvent.name();
+            const symbol = await ticketEvent.symbol();
+
+            expect(name).to.eq('TicketEvent');
+            expect(symbol).to.eq('TICK');
+        })
+    })
+})
