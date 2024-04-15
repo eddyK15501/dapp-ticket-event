69103ae3ba034db92763da0cb1d0a27bdd4cc97a
Edward Kim <itgma1231@gmail.com>
2024-04-14 22:12:59 -0400

    App.jsx - connect window.ethereum to hardhat accounts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,39 @@
-import { useState } from 'react'
-import { ethers } from 'ethers'
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 
 // ABIs
-import TicketEvent from '../abi/TicketEvent.json'
+import TicketEvent from '../abi/TicketEvent.json';
 
 // Config
-import config from '../config/config.json'
+import config from '../config/config.json';
 
 function App() {
+  const [account, setAccount] = useState(null);
+
+  const fetchData = async () => {
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+      const checksumAccount = ethers.utils.getAddress(accounts[0]);
+      setAccount(checksumAccount);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <div>
+        <header>
+          <h2 className='header__title'><strong>Event</strong> Tickets</h2>
+        </header>
         <h1>Client Start</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
