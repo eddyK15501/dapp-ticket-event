1f482620bb9f445e9bcb57b1f056e944adc5d9a3
Edward Kim <itgma1231@gmail.com>
2024-04-18 17:22:52 -0400

    SeatChart.jsx - initial JSX elements and styling added

--- client/src/components/SeatChart.jsx
+++ client/src/components/SeatChart.jsx
@@ -2,8 +2,26 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
+import closeIcon from '../assets/close (1).svg'
+
 const SeatChart = ({ setToggle, occasion, ticketEvent, provider }) => {
-  return <div>SeatChart</div>;
+  return <div className='occasion'>
+    <div className="occasion__seating">
+        <h1>{occasion.name} Seating Map</h1>
+        <button className='occasion__close'>
+            <img src={closeIcon} alt="Close Icon" />
+        </button>
+        <div className="occasion__stage">
+            <strong>STAGE</strong>
+        </div>
+        <div className='occasion__spacer--1'>
+            <strong>WALKWAY</strong>
+        </div>
+        <div className='occasion__spacer--2'>
+            <strong>WALKWAY</strong>
+        </div>
+    </div>
+  </div>;
 };
 
 export default SeatChart;
