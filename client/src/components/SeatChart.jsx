862ff8f3f2c75bada65a641eb5fd7b4a4e669e0d
Edward Kim <itgma1231@gmail.com>
2024-04-18 23:51:40 -0400

    SeatChart.jsx - contract.getSeatsTaken(_id) in useEffect

--- client/src/components/SeatChart.jsx
+++ client/src/components/SeatChart.jsx
@@ -1,27 +1,38 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-import closeIcon from '../assets/close (1).svg'
+import closeIcon from '../assets/close (1).svg';
 
 const SeatChart = ({ setToggle, occasion, ticketEvent, provider }) => {
-  return <div className='occasion'>
-    <div className="occasion__seating">
+  const [seatTaken, setSeatTaken] = useState(false);
+
+  useEffect(() => {
+    const getSeatsTaken = async () => {
+      const seating = await ticketEvent.getSeatsTaken(occasion.id);
+      setSeatTaken(seating);
+    };
+  }, []);
+
+  return (
+    <div className='occasion'>
+      <div className='occasion__seating'>
         <h1>{occasion.name} Seating Map</h1>
         <button className='occasion__close'>
-            <img src={closeIcon} alt="Close Icon" />
+          <img src={closeIcon} alt='Close Icon' />
         </button>
-        <div className="occasion__stage">
-            <strong>STAGE</strong>
+        <div className='occasion__stage'>
+          <strong>STAGE</strong>
         </div>
         <div className='occasion__spacer--1'>
-            <strong>WALKWAY</strong>
+          <strong>WALKWAY</strong>
         </div>
         <div className='occasion__spacer--2'>
-            <strong>WALKWAY</strong>
+          <strong>WALKWAY</strong>
         </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default SeatChart;
