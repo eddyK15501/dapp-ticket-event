27c801743953e3b73e54b62ca0b59d3a788b64fb
Edward Kim <itgma1231@gmail.com>
2024-04-19 00:24:49 -0400

    SeatChart.jsx - call mint() as signer, when seat for an event is bought

--- client/src/components/SeatChart.jsx
+++ client/src/components/SeatChart.jsx
@@ -1,24 +1,35 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from 'react';
-
+import Seat from './Seat';
 import closeIcon from '../assets/close (1).svg';
 
 const SeatChart = ({ setToggle, occasion, ticketEvent, provider }) => {
   const [seatTaken, setSeatTaken] = useState(false);
+  const [hasSold, setHasSold] = useState(false);
+
+  const onBuyHandler = async (seatId) => {
+    const signer = await provider.getSigner();
+    const transaction = await ticketEvent.connect(signer).mint(occasion.id, seatId, { value: occasion.cost });
+    await transaction.wait();
+    setHasSold(true);
+  }
 
   useEffect(() => {
     const getSeatsTaken = async () => {
       const seating = await ticketEvent.getSeatsTaken(occasion.id);
       setSeatTaken(seating);
     };
-  }, []);
+
+    getSeatsTaken();
+  }, [hasSold]);
 
   return (
     <div className='occasion'>
       <div className='occasion__seating'>
         <h1>{occasion.name} Seating Map</h1>
-        <button className='occasion__close'>
+        <button className='occasion__close' onClick={() => setToggle(false)}>
           <img src={closeIcon} alt='Close Icon' />
         </button>
         <div className='occasion__stage'>
