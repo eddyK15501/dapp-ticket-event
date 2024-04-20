45cd06a17dc1fb3c556028a3ee2f298cadbfc44f
Edward Kim <itgma1231@gmail.com>
2024-04-19 21:17:44 -0400

    Seat.jsx complete

--- client/src/components/Seat.jsx
+++ client/src/components/Seat.jsx
@@ -12,7 +12,22 @@ const Seat = ({
   seatTaken,
   onBuyHandle,
 }) => {
-  return <div>Seat</div>;
+  return (
+    <div
+      className={
+        seatTaken.find((seat) => Number(seat) == i + step)
+          ? 'occasion__seats--taken'
+          : 'occasion__seats'
+      }
+      style={{
+        gridColumn: `${(i % maxColumns) + 1 + columnStart}`,
+        gridRow: `${Math.ceil((i + 1) / maxRows) + rowStart}`,
+      }}
+      onClick={() => onBuyHandle(i + step)}
+    >
+      {i + step}
+    </div>
+  );
 };
 
 export default Seat;
